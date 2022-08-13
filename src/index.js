867c559bd6aa28fdf776163374709646608782bc
Kalinkina Elizaveta <kaliveta@gmail.com>
2022-08-13 08:16:22 +0800

    feat:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+
+	let array = str.split('');
+	const close = {};
+	bracketsConfig.forEach(el => {
+		close[el[1]] = el[0]
+	})
+
+	let stack = [];
+
+	for (let i = 0; i < array.length; i++) {
+		let topElement = stack[stack.length - 1];
+		let current = array[i];
+		if (stack.length && topElement === close[current]) { stack.pop(); } else {
+			stack.push(current);
+		}
+	}
+	return stack.length === 0;
 }
+
